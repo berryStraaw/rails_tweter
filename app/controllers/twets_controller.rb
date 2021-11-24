462c73e43fea380ad77450c8db9c3baa7f80be94
berryStraw <marius1357975@gmail.com>
2021-11-24 15:27:18 +0000

    added base files

--- app/controllers/twets_controller.rb
+++ app/controllers/twets_controller.rb
@@ -3,7 +3,8 @@ class TwetsController < ApplicationController
 
   # GET /twets or /twets.json
   def index
-    @twets = Twet.all
+    @twets = Twet.all.order("created_at DESC")
+    @twet = Twet.new
   end
 
   # GET /twets/1 or /twets/1.json
